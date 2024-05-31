bc9be2a3777a4ad75ecff70fbfbb223b738affa6
isa <isa430752@gmail.com>
2024-05-31 22:07:14 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+
+const slides = document.querySelectorAll('.slider img'); // select all images
+const prevBtn = document.querySelector(".prev-btn"); // select previous button
+const nextBtn = document.querySelector(".next-btn"); // select next button
+let galleryContainer = document.querySelector(".gallery-container"); //select gallery container
+galleryContainer.style.gridTemplateColumns = `repeat(${slides.length}, 1fr)`; // set grid template columns
+let currentSlide = 0; // set current slide to 0
+updateSliderCounter(); // update slider counter function triggered
+// function to go to slide
+function goToSlide(n) {
+
+  slides[currentSlide].classList.remove('active'); // remove active class from current slide
+  currentSlide = (n + slides.length) % slides.length; // set current slide to n
+  slides[currentSlide].classList.add('active'); // add active class to current slide
+  updateSliderCounter(); // update slider counter function triggered
+  updateThumbnailActiveStatus(currentSlide); // update thumbnail active status function triggered
+
+}
+
+// add event listener to previous button
+prevBtn.addEventListener("click", () => {
+  goToSlide(currentSlide - 1); 
+});
+
+// add event listener to next button
+nextBtn.addEventListener("click", () => {
+  goToSlide(currentSlide + 1);
+});
+
+// function to update slider counter
+function  updateSliderCounter(){
+  prevBtn.disabled = currentSlide === 0;  
+  nextBtn.disabled = currentSlide === slides.length - 1;  
+}
+
+// function to update thumbnail active status
+slides.forEach((img ,index) =>{  
+
+  const thumbnail = img.cloneNode(); // clone image
+  thumbnail.addEventListener("click", () => {
+    goToSlide(index); // go to slide
+    thumbnail.classList.add("active"); // add active class to thumbnail
+  });
+  galleryContainer.appendChild(thumbnail); // append thumbnail to gallery container
+});
+
+// function to update thumbnail active status
+function  updateThumbnailActiveStatus(index){
+  galleryContainer.querySelectorAll("img").forEach((img , i ) => {
+    img.classList.toggle("active" , i === index) 
+  });
+  }
